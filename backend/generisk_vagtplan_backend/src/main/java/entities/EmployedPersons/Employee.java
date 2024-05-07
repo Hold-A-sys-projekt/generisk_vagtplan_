fa73c6b1fce75d604885bc63192693ea910e7356
veronikaMinddal <cph-vm58@cphbusiness.dk>
2024-05-07 14:28:26 +0200

    creates Manager and employee

---
+++ backend/generisk_vagtplan_backend/src/main/java/entities/EmployedPersons/Employee.java
@@ -0,0 +1,5 @@
+public class Employee extends EmployedPerson {
+    public Employee (String name, String email, String username, String password, int id) {
+        super(name, email, username, password, id);
+    }
+}
---
+++ backend/generisk_vagtplan_backend/src/main/java/entities/EmployedPersons/Manager.java
@@ -0,0 +1,5 @@
+public class Manager extends EmployedPerson {
+    public Manager (String name, String email, String username, String password, int id) {
+        super(name, email, username, password, id);
+    }
+}

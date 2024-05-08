e21cbad7458ac058183f1f0637c7aa76b43b9c8e
TheRealJackiBoi <jslam@oulund.dk>
2024-05-08 10:53:55 +0200

    users table

--- frontend/src/pages/UserAdminPage.jsx
+++ frontend/src/pages/UserAdminPage.jsx
@@ -1,10 +1,38 @@
+import { Card } from "@/components/ui/card";
+import {
+  Table,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableBody,
+} from "@/components/ui/table";
 
 const UserAdminPage = () => {
-
   return (
     <div>
       <h1>User Admin Page</h1>
+      <div className="container my-4">
+        <Card>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Email</TableHead>
+                <TableHead>Navn</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>email@gmail.com</TableCell>
+                <TableCell>Navn Navnesen</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
+      </div>
     </div>
-  )}
+  );
+};
 
 export default UserAdminPage;

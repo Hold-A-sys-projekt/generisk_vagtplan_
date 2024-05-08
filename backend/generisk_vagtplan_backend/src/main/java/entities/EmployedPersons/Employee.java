47fb45615cfa8fb1d229b1ba0e5a2ea602d078f7
cph-sl444 <cph-sl444@cphbusiness.dk>
2024-05-08 13:02:14 +0200

    deleted something
    deleted something

--- backend/generisk_vagtplan_backend/src/main/java/entities/EmployedPersons/Employee.java
+++
@@ -1,8 +0,0 @@
-/*
-package entities.EmployedPersons;
-
-public class Employee extends EmployedPerson {
-    public Employee (String name, String email, String username, String password, int id) {
-        super(name, email, username, password, id);
-    }
-}*/
--- backend/generisk_vagtplan_backend/src/main/java/entities/EmployedPersons/Manager.java
+++
@@ -1,8 +0,0 @@
-/*
-package entities.EmployedPersons;
-
-public class Manager extends EmployedPerson {
-    public Manager (String name, String email, String username, String password, int id) {
-        super(name, email, username, password, id);
-    }
-}*/

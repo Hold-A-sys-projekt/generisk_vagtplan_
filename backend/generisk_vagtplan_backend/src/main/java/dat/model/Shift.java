9b4cdf5f2739b8f0820fd98952d64552f46d060f
VivekNagra <71701790+VivekNagra@users.noreply.github.com>
2024-05-08 14:31:42 +0200

    shift and shiftswap entity

---
+++ backend/generisk_vagtplan_backend/src/main/java/dat/model/Shift.java
@@ -0,0 +1,47 @@
+package dat.model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Shift {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "shift_name", nullable = false)
+    private String shiftName;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "shift1", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SwapShifts> swapsAsShift1;
+
+    @OneToMany(mappedBy = "shift2", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SwapShifts> swapsAsShift2;
+
+    public Shift(String shiftName, LocalDateTime startTime, LocalDateTime endTime, User user) {
+        this.shiftName = shiftName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.user = user;
+    }
+}

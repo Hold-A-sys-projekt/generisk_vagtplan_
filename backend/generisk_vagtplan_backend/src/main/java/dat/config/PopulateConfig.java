951aba7e9f57a07d2c27df40b7e109edeb6c18ee
THCSC <tobias@skancode.dk>
2024-05-09 13:12:45 +0200

    update PopulateConfig to create multiple departments and also a company

--- backend/generisk_vagtplan_backend/src/main/java/dat/config/PopulateConfig.java
+++ backend/generisk_vagtplan_backend/src/main/java/dat/config/PopulateConfig.java
@@ -1,7 +1,9 @@
 package dat.config;
 
+import dat.dao.CompanyDAO;
 import dat.dao.DepartmentDAO;
 import dat.dao.UserDAO;
+import dat.model.Company;
 import dat.model.Department;
 import dat.model.User;
 
@@ -10,13 +12,18 @@ public class PopulateConfig {
     public static void main(String[] args) {
         UserDAO userDao = UserDAO.getInstance();
         DepartmentDAO departmentDao = DepartmentDAO.getInstance();
+        CompanyDAO companyDAO = CompanyDAO.getInstance();
 
         try {
             userDao.registerUser("john@gmail.com", "john", "1234", "employee");
             userDao.registerUser("karen@gmail.com", "karen", "1234", "employee");
             userDao.registerUser("admin@admin.com", "admin", "admin", "manager");
+            User companyAdmin = userDao.registerUser("admin@company.com", "companyAdmin", "admin", "company_admin");
 
-            Department dep = departmentDao.create(new Department("Marketing"));
+            Company company = companyDAO.create(new Company(companyAdmin));
+            Department dep = departmentDao.create(new Department("Marketing", company));
+            departmentDao.create(new Department("Sales", company));
+            departmentDao.create(new Department("production", company));
             userDao.readAll().forEach(user -> {
                 user.setDepartment(dep);
                 userDao.update(user);

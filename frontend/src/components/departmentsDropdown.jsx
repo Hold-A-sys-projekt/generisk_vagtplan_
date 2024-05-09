dc68202d74166fcfe08766c2452ac8d9a782c450
Julius Lassen <julius5517@gmail.com>
2024-05-09 12:26:17 +0200

    small changes

--- frontend/src/components/departmentsDropdown.jsx
+++ frontend/src/components/departmentsDropdown.jsx
@@ -1,34 +1,46 @@
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
+  DropdownMenuRadioGroup, DropdownMenuRadioItem
 } from '@/components/ui/dropdown-menu';
 import { useEffect, useState } from 'react';
 import { Button } from './ui/button';
+import { getAllDepartments } from '@/lib/departmentFacade';
 
 const DepartmentsDropdown = ({ defaultValue }) => {
   const [departments, setDepartments] = useState([]);
 
-  const loadDepartments = async () => {
+  const [selectedDepartment, setSelectedDepartment] = useState(defaultValue);
 
+  const loadDepartments = async () => {
+    setDepartments(await getAllDepartments());
   }
 
+  const tests = [
+    {id: 1, name: "Marketing"}
+    , {id:2, name: "Finance"}
+    ,{id:3 ,name: "Human Resources"}];
+
   useEffect(() => {
-    loadDepartments()
-  })
+    //loadDepartments()
+    setDepartments(tests)
+    
+  }, [])
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <Button>{defaultValue ? defaultValue : "Vælg Lokation"}</Button>
+        <Button variant="secondary">{defaultValue ? defaultValue : "Vælg Lokation"}</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={selectedDepartment} onValueChange={setSelectedDepartment}>
         {departments.map((department) => (
-          <DropdownMenuItem key={department.id}>{department.name}</DropdownMenuItem>
+          <DropdownMenuRadioItem key={department.id}>{department.name}</DropdownMenuRadioItem>
         ))}
+        </DropdownMenuRadioGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );

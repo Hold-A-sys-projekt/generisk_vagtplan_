2f76052fc286fe6eef05a3b8f292a5733b955472
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2024-05-13 14:28:31 +0200

    Temporary routing for punch component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Index from '@/pages/Index.jsx'
+import Punch from "@/components/punch.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Index />} />
+        <Route path="punch" element={<Punch />} />
     </Route>
   )
 )

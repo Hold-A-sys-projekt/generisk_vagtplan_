0eeefeffb630e2f7de07ba21367d7f8f3c853ff1
Caner Turan <91967050+Turan10@users.noreply.github.com>
2024-05-14 00:08:11 +0200

    frontend

--- frontend/src/components/ShiftSwapManager.jsx
+++ frontend/src/components/ShiftSwapManager.jsx
@@ -17,34 +17,41 @@ const ShiftSwapManager = () => {
 
   useEffect(() => {
     axios.get('http://localhost:7070/api/swapshifts')
-      .then(response => {
-        const sortedData = response.data.sort((a, b) => (a.isAccepted === null) ? -1 : ((b.isAccepted === null) ? 1 : 0));
-        setShiftSwaps(sortedData.map(swap => ({
-          id: swap.shiftId,
-          shift1Day: formatDate(swap.shift1.startTime),
-          shift1User: swap.shift1.user.username,
-          shift1Time: `${formatTime(swap.shift1.startTime)} - ${formatTime(swap.shift1.endTime)}`,
-          shift2Day: formatDate(swap.shift2.startTime),
-          shift2User: swap.shift2.user.username,
-          shift2Time: `${formatTime(swap.shift2.startTime)} - ${formatTime(swap.shift2.endTime)}`,
-          isAccepted: swap.isAccepted
-        })));
-      })
-      .catch(error => console.error('There was an error fetching the shift swaps', error));
+        .then(response => {
+          const sortedData = response.data.sort((a, b) => (a.isAccepted ? 1 : -1));
+          setShiftSwaps(sortedData.map(swap => ({
+            id: swap.shiftId,
+            shift1Day: formatDate(swap.shift1.startTime),
+            shift1User: swap.shift1.user.username,
+            shift1Time: `${formatTime(swap.shift1.startTime)} - ${formatTime(swap.shift1.endTime)}`,
+            shift2Day: formatDate(swap.shift2.startTime),
+            shift2User: swap.shift2.user.username,
+            shift2Time: `${formatTime(swap.shift2.startTime)} - ${formatTime(swap.shift2.endTime)}`,
+            isAccepted: swap.isAccepted || 'Pending'
+          })));
+        })
+        .catch(error => console.error('There was an error fetching the shift swaps', error));
   }, []);
 
   const handleSwapApproval = (index, isApproved) => {
-    const swap = shiftSwaps[index];
-    const updatedSwaps = [...shiftSwaps];
-    updatedSwaps[index].isAccepted = isApproved ? 'Approved' : 'Not Approved';
-    setShiftSwaps(updatedSwaps.sort((a, b) => (a.isAccepted === null) ? -1 : ((b.isAccepted === null) ? 1 : 0)));
+    axios.post(`http://localhost:7070/api/swapshifts/${shiftSwaps[index].id}/approve`, { isAccepted: isApproved ? 'Approved' : 'Not Approved' })
+        .then(() => {
+          setShiftSwaps(prevSwaps => {
+            const newSwaps = [...prevSwaps];
+            newSwaps[index].isAccepted = isApproved ? 'Approved' : 'Not Approved';
+            return newSwaps.sort((a, b) => (a.isAccepted ? 1 : -1));
+          });
+        })
+        .catch(error => {
+          console.error(`Failed to ${isApproved ? 'approve' : 'decline'} shift swap`, error);
+        });
   };
 
   return (
-    <div className="max-w-6xl mx-auto mt-10">
-      <h1 className="text-2xl font-bold text-center mb-6">Shift Swap Requests</h1>
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead>
+      <div className="max-w-6xl mx-auto mt-10">
+        <h1 className="text-2xl font-bold text-center mb-6">Shift Swap Requests</h1>
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead>
           <tr>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Shift 1 Day</th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Shift 1 User</th>
@@ -55,30 +62,30 @@ const ShiftSwapManager = () => {
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
           </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
           {shiftSwaps.map((swap, index) => (
-            <tr key={index}>
-              <td className="px-6 py-4 whitespace-nowrap">{swap.shift1Day}</td>
-              <td className="px-6 py-4 whitespace-nowrap">{swap.shift1User}</td>
-              <td className="px-6 py-4 whitespace-nowrap">{swap.shift1Time}</td>
-              <td className="px-6 py-4 whitespace-nowrap">{swap.shift2Day}</td>
-              <td className="px-6 py-4 whitespace-nowrap">{swap.shift2User}</td>
-              <td className="px-6 py-4 whitespace-nowrap">{swap.shift2Time}</td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${swap.isAccepted ? (swap.isAccepted === 'Approved' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800') : 'bg-gray-100 text-gray-500'}`}>
-                  {swap.isAccepted || 'Pending'}
+              <tr key={index} className={`${swap.isAccepted !== 'Pending' ? 'bg-gray-200' : ''}`}>
+                <td className="px-6 py-4 whitespace-nowrap">{swap.shift1Day}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{swap.shift1User}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{swap.shift1Time}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{swap.shift2Day}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{swap.shift2User}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{swap.shift2Time}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${swap.isAccepted === 'Approved' ? 'bg-green-100 text-green-800' : (swap.isAccepted === 'Not Approved' ? 'bg-red-100 text-red-800' : 'bg-gray-100 text-gray-500')}`}>
+                  {swap.isAccepted}
                 </span>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <button onClick={() => handleSwapApproval(index, true)} className="text-white bg-green-500 hover:bg-green-600 font-medium rounded-lg text-sm px-3 py-1 mr-2">Approve</button>
-                <button onClick={() => handleSwapApproval(index, false)} className="text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-3 py-1">Decline</button>
-              </td>
-            </tr>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                  <button onClick={() => handleSwapApproval(index, true)} disabled={swap.isAccepted !== 'Pending'} className={`text-white bg-green-500 hover:bg-green-600 font-medium rounded-lg text-sm px-3 py-1 mr-2 ${swap.isAccepted !== 'Pending' ? 'opacity-50 cursor-not-allowed' : ''}`}>Approve</button>
+                  <button onClick={() => handleSwapApproval(index, false)} disabled={swap.isAccepted !== 'Pending'} className={`text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-3 py-1 ${swap.isAccepted !== 'Pending' ? 'opacity-50 cursor-not-allowed' : ''}`}>Decline</button>
+                </td>
+              </tr>
           ))}
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
   );
 };
 

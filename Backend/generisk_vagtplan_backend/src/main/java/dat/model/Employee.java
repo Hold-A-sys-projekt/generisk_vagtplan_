40bcdf88d2ca5be7b7651b3d2322783b1fbfe88b
Toebzy <Tobiascarlsen@live.dk>
2024-05-15 10:38:05 +0200

    cascade fix

--- Backend/generisk_vagtplan_backend/src/main/java/dat/model/Employee.java
+++ Backend/generisk_vagtplan_backend/src/main/java/dat/model/Employee.java
@@ -24,10 +24,11 @@ public class Employee implements dat.model.Entity<EmployeeDTO> {
     private int id;
 
     private String name;
-
+  
     private String role;
 
-    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+ 
     private Set<Shift> shifts = new LinkedHashSet<>();
 
     public Employee(String name) {

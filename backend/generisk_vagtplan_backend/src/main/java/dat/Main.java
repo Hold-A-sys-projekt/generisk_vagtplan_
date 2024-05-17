8ad3fb3c8f2dcab0dc47a094bbba342e961e7548
Peter537 <pean@outlook.dk>
2024-05-17 23:34:13 +0200

    Update Main.java

--- backend/generisk_vagtplan_backend/src/main/java/dat/Main.java
+++ backend/generisk_vagtplan_backend/src/main/java/dat/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationConfig.startServer(7070);
+        System.out.println("Server started on " + ApplicationConfig.getBaseURL());
     }
 }

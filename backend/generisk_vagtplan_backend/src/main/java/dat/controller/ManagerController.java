3ccf40cc5bd445a76fda3117b00eeec9b06b42f8
Julius Lansner <axiiss@live.dk>
2024-05-21 13:21:27 +0200

    Revert "Merge branch '25-backend' into 25-frontend-bu"
    This reverts commit 9cd7c3ae8ed2f8dc686cd62c195269252140ad6c, reversing
    changes made to ab426423f0852f4d196413605be5be740000127f.

--- backend/generisk_vagtplan_backend/src/main/java/dat/controller/ManagerController.java
+++ backend/generisk_vagtplan_backend/src/main/java/dat/controller/ManagerController.java
@@ -1,14 +1,13 @@
 package dat.controller;
 
 import dat.dao.ManagerDAO;
-import dat.dto.EmployeeDTO;
-import dat.dto.ManagerDTO;
-import dat.model.Employee;
-import dat.model.Manager;
+import dat.dto.UserDTO;
+import dat.model.Role;
+import dat.model.User;
 import io.javalin.http.Context;
 
 
-public class ManagerController extends Controller<Manager, ManagerDTO> {
+public class ManagerController extends Controller<User, UserDTO> {
     private final ManagerDAO dao;
 
     public ManagerController(ManagerDAO dao) {
@@ -17,10 +16,10 @@ public class ManagerController extends Controller<Manager, ManagerDTO> {
     }
 
     public void addEmployee(Context ctx) {
-        EmployeeDTO employeeDTO = ctx.bodyAsClass(EmployeeDTO.class);
-        Employee employee = new Employee();
-        employee.setName(employeeDTO.getName());
-        employee.setRole(employeeDTO.getRole());
+        UserDTO employeeDTO = ctx.bodyAsClass(UserDTO.class);
+        User employee = new User();
+        employee.setUsername(employeeDTO.getUsername());
+        employee.setRole(employeeDTO.getRole().toEntity());
         dao.addEmployee(employee);
         ctx.status(201);
     }
@@ -28,12 +27,12 @@ public class ManagerController extends Controller<Manager, ManagerDTO> {
     public void updateEmployeeRole(Context ctx) {
         int employeeId = Integer.parseInt(ctx.pathParam("id"));
         try {
-            EmployeeDTO requestBody = ctx.bodyAsClass(EmployeeDTO.class);
-            String newRole = requestBody.getRole();
+            UserDTO requestBody = ctx.bodyAsClass(UserDTO.class);
+            String newRole = requestBody.getRole().getName();
 
-            Employee employee = dao.getEmployee(employeeId);
+            User employee = dao.getEmployee(employeeId);
             if (employee != null) {
-                employee.setRole(newRole);
+                employee.setRole(new Role(newRole));
                 dao.updateEmployeeRole(employee, newRole);
                 ctx.status(200).json(employee);
             } else {

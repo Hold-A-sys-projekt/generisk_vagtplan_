b97f010b1a7c0b72cf131a6ba1acc487485a22fd
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2024-05-22 11:12:01 +0200

    Manager receives email when user try buying a shift

--- backend/generisk_vagtplan_backend/src/main/java/dat/controller/BuyRequestController.java
+++ backend/generisk_vagtplan_backend/src/main/java/dat/controller/BuyRequestController.java
@@ -1,6 +1,5 @@
 package dat.controller;
 
-import dat.config.HibernateConfig;
 import dat.dao.DAO;
 import dat.dao.ShiftDAO;
 import dat.dao.UserDAO;
@@ -9,10 +8,12 @@ import dat.exception.ApiException;
 import dat.model.BuyRequest;
 import dat.model.Shift;
 import dat.model.User;
+import dat.util.EmailSender;
 import io.javalin.http.Context;
 
-public class BuyRequestController extends Controller<BuyRequest, BuyRequestDTO>
-{
+import java.util.List;
+
+public class BuyRequestController extends Controller<BuyRequest, BuyRequestDTO> {
     private UserDAO userDAO;
     private ShiftDAO shiftDAO;
 
@@ -37,6 +38,20 @@ public class BuyRequestController extends Controller<BuyRequest, BuyRequestDTO>
         BuyRequest buyRequest = new BuyRequest(user, shift);
 
         dao.create(buyRequest);
+
+        List<User> managers = user.getDepartment().getUsers()
+                .stream()
+                .filter(u -> u.getRole().getName().equals("manager"))
+                .toList();
+
+        if (!managers.isEmpty()) {
+            for (User manager : managers) {
+                EmailSender.sendEmail(manager.getEmail(), "Buy request",
+                        List.of("User " + user.getEmail() +
+                                " has requested to buy shift " + shift.getId()
+                                + ".\nPlease approve or deny the request in the system."), false);
+            }
+        }
 
         ctx.status(201);
         ctx.json(buyRequest.toDTO());

7b3d14fcc17fe026257534274fa3184a329f5553
Peter537 <pean@outlook.dk>
2024-05-24 11:58:30 +0200

    Update ShiftSale.jsx

--- frontend/src/pages/ShiftSale.jsx
+++ frontend/src/pages/ShiftSale.jsx
@@ -1,40 +1,32 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import ShiftsSaleList from "../components/shiftsSaleList";
 import React from "react";
 
-
 const ShiftSale = () => {
+  const user = {
+    role_name: "employee",
+    // implement logic to get the user role
+  };
 
-    const user = {
-        role_name: "employee"
-        // implement logic to get the user role
-    }
+  return (
+    <div className="flex flex-col h-screen p-4 pt-0">
+      <div className="border-t border-gray-400 my-2"></div>
 
-    return (
-        <div className="flex flex-col h-screen p-4 pt-0">
+      <h2 className="mb-5 mt-4 text-5xl font-semibold tracking-tight text-gray-900 dark:text-white">
+        Shifts for Sale
+      </h2>
 
-            <div className="border-t border-gray-400 my-2" ></div>
-gi
-            <h2 className="mb-5 mt-4 text-5xl font-semibold tracking-tight text-gray-900 dark:text-white">Shifts for Sale</h2>
+      <div className="flex-grow text-center p-10 bg-gray-200 mb-4 mt-4">
+        <ShiftsSaleList user={user} />
+      </div>
 
-            <div className="flex-grow text-center p-10 bg-gray-200 mb-4 mt-4">
-                <ShiftsSaleList  user={user} />
-            </div>
-
-            <div className="flex items-center justify-center text-center p-4 h-60 bg-gray-200">
-                <Link to="" className="text-blue-500 underline">
-                    <p>TEST</p>
-                </Link>
-            </div>
-
-        </div>
-
-
-    )
-}
-
-
-
-
+      <div className="flex items-center justify-center text-center p-4 h-60 bg-gray-200">
+        <Link to="" className="text-blue-500 underline">
+          <p>TEST</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default ShiftSale;

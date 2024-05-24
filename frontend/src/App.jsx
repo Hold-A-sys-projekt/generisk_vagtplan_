4059705521f1a987cdcd6381b69377c390345270
Peter537 <pean@outlook.dk>
2024-05-24 11:58:10 +0200

    Removed import that didnt work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-import { Outlet } from 'react-router-dom';
-import './App.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Outlet } from "react-router-dom";
+import "./App.css";
 
-import Navbar from './components/navbar';
-import { Toaster } from './components/ui/toaster';
+import Navbar from "./components/navbar";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
